972418aa736078bb208dc7b0e038a615e8e7149f
Alex-Devonte <alexdevontetaylor@gmail.com>
2019-01-24 19:17:13 -0500

    Added logic for calculations

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  model: any = {};
   constructor() { }
 
   ngOnInit() {
+    
+  }
+  
+  getLoanBalance(bal) {
+    console.log("Balance: " + bal)  ;
   }
 
+  calculateLoanPayoff()  {
+    var monthlyInterest = this.calculateMonthlyInterest(this.model.APR);
+    var remainingPayments = this.calculateRemainingPayments(monthlyInterest, this.model.monthlyPayment, this.model.loanBalance);
+    var totalInterest = this.calculateTotalInterest(remainingPayments, this.model.monthlyPayment, this.model.loanBalance);
+    console.log(this.model);
+    console.log("Monthly Interest: " + monthlyInterest);
+    console.log("Remaining Payments: " + Math.round(remainingPayments));
+    console.log("Total Interest: " + totalInterest);
+    console.log(-Math.log(1-(0.5/100)*3500/100)/Math.log(1+ (0.5/100)));
+  }
+
+  calculateMonthlyInterest(APR) {
+    return APR / 12 / 100;
+  }
+
+  calculateRemainingPayments(monthlyInterest, monthlyPayment, principal) {
+    return (-Math.log(1-monthlyInterest * principal / monthlyPayment) / Math.log(1+monthlyInterest));
+  }
+
+  calculateTotalInterest(remainingPayments, monthlyPayment, principal) {
+    return Math.round((remainingPayments * monthlyPayment - principal));
+  }
 }

4d2320089a367bf31f2700afba393488ed547ca6
Alex-Devonte <alexdevontetaylor@gmail.com>
2019-01-27 22:27:52 -0500

    Added code for accessing form data

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,4 +1,6 @@
+import { Chart } from 'chart.js';
 import { Component, OnInit } from '@angular/core';
+import { DataTransferService } from '../data-transfer.service';
 
 @Component({
   selector: 'app-results',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent implements OnInit {
+  chart = [];
+  data = this.transferService.getData();
 
-  constructor() { }
+  constructor(private transferService: DataTransferService) { }
 
   ngOnInit() {
-  }
+    this.transferService.data.subscribe(data => {
+      this.data = data;
+    });
 
+    this.chart = new Chart('canvas', {
+      type: 'bar',
+      data: {
+        labels: "weatherDates".split(","),
+        datasets: [
+          { 
+            data: "temp_max",
+            borderColor: "#3cba9f",
+            fill: false
+          },
+          { 
+            data: "temp_min",
+            borderColor: "#ffcc00",
+            fill: false
+          },
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes: [{
+            display: true
+          }],
+        }
+      }
+    });
+  }
+  }
 }

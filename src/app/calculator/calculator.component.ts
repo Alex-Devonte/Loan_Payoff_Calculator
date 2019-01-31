c859c0a7925d34cee90c554c409e08b6fd4a6bf0
Alex-Devonte <alexdevontetaylor@gmail.com>
2019-01-31 16:35:12 -0500

    Added amortization schedule function

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -11,16 +11,15 @@ export class CalculatorComponent implements OnInit {
   private loadComponent = false;
   constructor(private transferService:DataTransferService) {}
 
-  ngOnInit() {
-    
-  }
+  ngOnInit() {}
 
   calculateLoanPayoff()  {
     this.model.monthlyInterest = this.calculateMonthlyInterest(this.model.APR);
-    this.model.remainingPayments = this.calculateRemainingPayments(this.model.monthlyInterest, this.model.monthlyPayment, this.model.loanBalance);
+    this.model.remainingPayments = Math.round(this.calculateRemainingPayments(this.model.monthlyInterest, this.model.monthlyPayment, this.model.loanBalance));
     this.model.totalInterest = this.calculateTotalInterest(this.model.remainingPayments, this.model.monthlyPayment, this.model.loanBalance);
-    this.model.yearsLeft = (this.model.remainingPayments/12);
-   
+    this.model.yearsLeft = (this.model.remainingPayments/12).toFixed(1);
+    this.calculateAmortizationSchedule();
+
     this.transferService.setData(this.model);
   }
 
@@ -34,6 +33,43 @@ export class CalculatorComponent implements OnInit {
 
   calculateTotalInterest(remainingPayments, monthlyPayment, principal) {
     return Math.round((remainingPayments * monthlyPayment - principal));
+  }
+
+  calculateAmortizationSchedule() {
+    var balance = this.model.loanBalance;
+    var totalInterest = 0;
+
+    var data = {};
+    data['balance'] = new Array();
+    data['interest'] = new Array();
+
+    // Calculate the amortization  schedule
+    for (var count = 0; count <= Math.round(this.model.remainingPayments);count++)
+    {
+      var interest = 0;
+      var monthlyPrincipal = 0;
+
+      /* Push values to array if count is a multiple of 12
+         At the moment we only need data per year */
+      if (count % 12 == 0)
+      {
+        data['balance'].push(Math.round(balance));
+        data['interest'].push(Math.round(totalInterest));
+      }
+
+      //Get interest amount
+      interest = balance * this.model.monthlyInterest;
+
+      //Keep track of accruing monthly interest
+      totalInterest += interest;   
+
+      monthlyPrincipal = this.model.monthlyPayment - interest;
+
+      //Keep track of remaing monthly balance
+      balance -= monthlyPrincipal;
+    }
+    console.log(data);
+    this.model.data = data;
   }
 
   showResults() {
